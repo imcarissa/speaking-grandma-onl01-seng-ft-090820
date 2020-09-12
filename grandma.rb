4fb7aee27db9ebdfc22d6e8ac5d49df741dd46ee
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-12 04:59:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,11 +8,11 @@ def speak_to_grandma(phrase)
 
 if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
-elsif phrase == phrase.downcase
-  return "HUH?! SPEAK UP, SONNY!"
 elsif phrase == phrase.upcase
-   puts "NO, NOT SINCE 1938!"
+   return "NO, NOT SINCE 1938!"
 else
   return "NO, NOT SINCE 1938!"
+  return "HUH?! SPEAK UP, SONNY!"
+else
   end
 end
