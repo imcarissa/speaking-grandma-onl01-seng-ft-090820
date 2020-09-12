5a380c5aa1e66b692a04a58c6f14feab6abb9cbe
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-12 04:20:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,22 +1,22 @@
 # Write a speak_to_grandma method.
 
-speak_to_grandma = "I LOVE YOU GRANDMA".upcase
+speak_to_grandma = "I LOVE YOU GRANDMA"
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-if speak_to_grandma("I LOVE YOU GRANDMA") == "I LOVE YOU GRANDMA!".upcase
-  puts "I LOVE YOU TOO PUMPKIN!".upcase
-elsif speak_to_grandma("Hi Nana") == "Hi, Nana. How are you?"
-  puts "HUH?! SPEAK UP, SONNY!".upcase
-elsif speak_to_grandma == "Hi!".capitalize
-  puts "HUH?! SPEAK UP, SONNY!".upcase
+if speak_to_grandma("I LOVE YOU GRANDMA".upcase) == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+elsif speak_to_grandma("Hi, Nana how are you?") == "Hi, Nana. How are you?"
+  puts "HUH?! SPEAK UP, SONNY!"
+elsif speak_to_grandma("Hi!") == "Hi!"
+  puts "HUH?! SPEAK UP, SONNY!"
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
-elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?".capitalize
-  puts "NO, NOT SINCE 1938!".capitalize
+elsif speak_to_grandma("WHAT DID YOU EAT TODAY?".capitalize) == "WHAT DID YOU EAT TODAY?"
+  puts "NO, NOT SINCE 1938!"
 
 # NO, NOT SINCE 1938!
 
