fede93cb4bb50bc3d883ef70c0e34abff02343a0
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-12 04:44:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@ if phrase = "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == phrase.downcase
   puts "HUH?! SPEAK UP, SONNY!"
-else phrase == phrase.upcase
+elsif phrase == phrase.upcase
   puts "NO, NOT SINCE 1938!"
 else
   return "NO, NOT SINCE 1938!"
