d50a5271fdc7f1f76008aea86b9281797e16e67b
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-12 04:54:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
 # unless you shout it (type in all capitals).
 
 if phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == phrase.downcase
   puts "HUH?! SPEAK UP, SONNY!"
 # elsif phrase == phrase.upcase
