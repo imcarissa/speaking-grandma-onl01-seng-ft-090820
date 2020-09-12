15bd9d48ce64c798a63f40160903211e30b8bdd9
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-12 03:56:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,14 +6,17 @@ speak_to_grandma = "ARE YOU HUNGRY, GRANDMA?!".upcase
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-
-
-elsif speak_to_grandma == "I LOVE YOU GRANDMA"
+if speak_to_grandma == "ARE YOU, HUNGRY, GRANDMA?!".upcase
+  puts "HUH?! SPEAK UP, SONNY!".upcase
+elsif speak_to_grandma == "I LOVE YOU GRANDMA".upcase
+  puts "I LOVE YOU TOO PUMPKIN".upcase
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+else speak_to_grandma == ""
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
